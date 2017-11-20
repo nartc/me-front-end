d3d97e8ee07ef29e5e60a4c8821877cc428ea94d
nartc <ctch5@mail.umsl.edu>
2017-11-19 21:50:45 -0600

    blah

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -1,15 +1,36 @@
 import {Injectable} from '@angular/core';
-import {Router, CanActivate} from '@angular/router';
+import { ActivatedRoute, CanActivate, GuardsCheckEnd, NavigationCancel, Router } from '@angular/router';
 import {AuthService} from '../services/auth.service';
 import {ClientService} from '../services/client.service';
+import 'rxjs/add/operator/filter';
 
 @Injectable()
 export class AuthGuard implements CanActivate{
+  private params: any;
   constructor(
       private authService:AuthService, 
       private router:Router,
-      private clientService: ClientService
+      private clientService: ClientService,
+      private aRoute: ActivatedRoute
     ){
+      // this.router.events.filter(event => event instanceof NavigationCancel)
+      //   .subscribe(
+      //     (e: NavigationCancel) => {
+      //       console.log(e.url.split("?"));
+      //     }
+      //   )
+
+      // this.router.events
+      //   .filter(event => event instanceof GuardsCheckEnd)
+      //   .subscribe((e: GuardsCheckEnd) => {
+      //    (async () => {
+      //       if(e.id === 1) {
+      //         let params = await e.state.root.queryParams;
+      //         console.log(params);
+      //       }
+      //    })();
+         
+      //   });
 
   }
 
@@ -17,7 +38,21 @@ export class AuthGuard implements CanActivate{
     if(this.authService.isLoggedIn() || this.clientService.isLoggedIn()){
       return true;
     } else  {
-      this.router.navigate(['/login']);
+      this.router.events.filter(event => event instanceof GuardsCheckEnd)
+        .subscribe(
+          (e: GuardsCheckEnd) => {
+            (async () => {
+              if(e.id === 1) {
+                this.params = await e.state.root.queryParams;
+              }
+            })()
+            .then(
+              () => {
+                this.router.navigate(['/login'], {queryParams: this.params, queryParamsHandling: "merge"})
+              }
+            );
+          }
+        );  
       return false;
     }
   }
